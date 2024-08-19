8cfad23aa6bc211c9f523651daa6d94b680cb05e
huy2000vu <huy2000vu@gmail.com>
2024-08-18 21:58:20 -0700

    added basic files

---
+++ script.js
@@ -0,0 +1,17 @@
+let num1, num2,op;
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+
+function operate(num1, op, num2){
+
+}
